8f445bf2aabc3d3b09c01e20dffb4bf810d1b570
DestinyPlayer <thenextdecade244@gmail.com>
2023-06-20 22:01:49 +0300

    Initial Commit
    Signed-off-by: DestinyPlayer <thenextdecade244@gmail.com>

---
+++ Source/SwapBuilding/SwapBuilding_Props.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using UnityEngine;
+using Verse;
+
+namespace RimWorld
+{
+    // Token: 0x020013E3 RID: 5091
+    public class CompPropertiesSwapBuilding_GF : CompProperties
+    {
+        // Token: 0x06007DB0 RID: 32176 RVA: 0x002B33ED File Offset: 0x002B15ED
+        public CompPropertiesSwapBuilding_GF()
+        {
+            this.compClass = typeof(CompSwapBuilding_GF);
+        }
+
+        // Token: 0x040046D2 RID: 18130
+        public ThingDef buildingSwap;
+        public string buttonName;
+        public string buttonDescription;
+        //public Texture2D uiIcon = BaseContent.BadTex;
+        public string uiIconPath;
+        public bool destroyOriginal = false;
+    }
+}
---
+++ Whatever/SwapBuilding.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using UnityEngine;
+using Verse;
+
+namespace RimWorld
+{
+    // Token: 0x020013E4 RID: 5092
+    public class CompSwapBuilding_BMU : ThingComp
+    {
+        // Token: 0x170015C2 RID: 5570
+        // (get) Token: 0x06007DB1 RID: 32177 RVA: 0x002B3405 File Offset: 0x002B1605
+        private CompPropertiesSwapBuilding_BMU Props
+        {
+            get
+            {
+                return (CompPropertiesSwapBuilding_BMU)this.props;
+            }
+        }
+        public bool ParentIsInactive
+        {
+            get
+            {
+                Building_MechUpgrader building_Upgrader = this.parent as Building_MechUpgrader;
+                if (building_Upgrader != null && building_Upgrader.State == SubcoreScannerState.Inactive)
+                {
+                    return true;
+                }
+                CompPawnSpawnOnWakeup compPawnSpawnOnWakeup = this.parent.TryGetComp<CompPawnSpawnOnWakeup>();
+                return compPawnSpawnOnWakeup != null && !compPawnSpawnOnWakeup.CanSpawn;
+            }
+        }
+        public bool PowerOn => parent.TryGetComp<CompPowerTrader>().PowerOn;
+        public override IEnumerable<Gizmo> CompGetGizmosExtra()
+        {
+            if (ParentIsInactive && this.PowerOn)
+            {
+                yield return new Command_Action
+                {
+                    defaultLabel = this.Props.buttonName,
+                    defaultDesc = this.Props.buttonDescription,
+                    icon = ContentFinder<Texture2D>.Get(this.Props.uiIconPath, true),//ContentFinder<Texture2D>.Get("UI/Gizmos/BMU_AllowCombatMechs", true),//this.Props.uiIcon,
+                    action = delegate ()
+                    {
+                        Thing thing2 = ThingMaker.MakeThing(this.Props.buildingSwap, null);
+                        Thing thing = GenSpawn.Spawn(thing2, parent.Position, parent.Map, parent.Rotation, WipeMode.Vanish, false);
+                       //Thing thing = GenSpawn.Spawn(this.Props.buildingSwap, parent.Position, parent.Map, parent.GetRotStage, WipeMode.Vanish, false);
+                      //  thing.Rotation = parent.Rotation;
+                        thing.HitPoints = parent.HitPoints;
+                        if (this.Props.destroyOriginal)
+                        {
+                            parent.Destroy();
+                        }
+                      
+                        thing.SetFaction(Faction.OfPlayer);
+                        // if (parent.def == this.Props.buildingSwap)
+                        // {
+                        // Thing thing = GenSpawn.Spawn(this.Props.buildingSwap, parent.Position, parent.Map);
+                        // thing.HitPoints = parent.HitPoints;
+                        //   parent.Destroy();
+                        // }
+                    }
+                };
+            }
+
+        }
+
+    }
+}
