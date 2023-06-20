0c754c01947096cdb3daa8d5dc27a67d09c12042
DestinyPlayer <thenextdecade244@gmail.com>
2023-06-21 00:52:35 +0300

    Attempted to add the ability to generate specifically Archite Genepacks.
    So far failed, hoping to figure it out EVENTUALLY.
    
    Signed-off-by: DestinyPlayer <thenextdecade244@gmail.com>

---
+++ Source/CategoricGenepacks/GF_ArchiteGenepack.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using UnityEngine;
+using Verse;
+
+namespace RimWorld
+{
+    public class GF_ArchiteGenepack : Genepack
+    {
+        public bool canBeArchite = true;
+        public override void PostMake()
+        {
+            base.PostMake();
+            geneSet = new GeneSet();
+
+            var allGeneDefs = DefDatabase<GeneDef>.AllDefsListForReading;
+            foreach (var def in allGeneDefs.InRandomOrder())
+            {
+                if (def.biostatArc > 0)
+                {
+                    geneSet.AddGene(def);
+                    break;
+                }
+            }
+            geneSet.GenerateName();
+        }
+    }
+}
